6c8747703c6634d82b20f9f1c1686bdb15f66944
Tom <Tom@PcTom>
2018-03-24 22:25:49 +0100

    Tom
    Shortest path fini!!!!!

--- src/myVelib/ShortestPath.java
+++ src/myVelib/ShortestPath.java
@@ -6,26 +6,48 @@ import myVelib.Station.Station;
 
 public class ShortestPath implements PathFinder {
 	
-	public Station startStation;
-	public Station endStation;
+	private Station startStation;
+	private Station endStation;
 	
 
 	@Override
 	public void Path(GPS start, GPS end, MyVelib velibNW) {
 		
 		double d=0;
+		Station startS = null;
+		Station endS = null;
 		for (int i=0; i<velibNW.getDistanceMap().length ;i++) {
 			
-			for (int j=i; j< velibNW.getDistanceMap()[i].length ;j++) {
+			for (int j=0; j< velibNW.getDistanceMap()[i].length ;j++) {
 				
-				double di = Math.sqrt((start.getX()-velibNW.getStations().get(i).getPosition().getX())^2+(si.getPosition().getY()-sj.getPosition().getY())^2);
-				double df = Math.sqrt((si.getPosition().getX()-sj.getPosition().getX())^2+(si.getPosition().getY()-sj.getPosition().getY())^2);
-				if(d==0 || distances[i][j]<d) {
+				double di = Math.sqrt((start.getX()-velibNW.getStations().get(i).getPosition().getX())^2+(start.getY()-velibNW.getStations().get(i).getPosition().getY())^2);
+				double df = Math.sqrt((end.getX()-velibNW.getStations().get(j).getPosition().getX())^2+(end.getY()-velibNW.getStations().get(j).getPosition().getY())^2);
+				
+				if(d==0 || di+df+velibNW.getDistanceMap()[i][j] < d) {
+					
+					startS=velibNW.getStations().get(i);
+					endS=velibNW.getStations().get(j);
+					d = di+df+velibNW.getDistanceMap()[i][j];
 					
 				}
 			}
+		this.startStation=startS;
+		this.endStation=endS;
+		
 		}
 	}
+
+
+	public Station getStartStation() {
+		return startStation;
+	}
+
+
+	public Station getEndStation() {
+		return endStation;
+	}
+	
+	
 	
 					
 					
