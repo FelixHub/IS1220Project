61efb0ca3f4353c3a5824d7ed1402885bc89b8c3
Tom <Tom@PcTom>
2018-03-25 01:23:24 +0100

    Tom
    fastest

--- src/myVelib/FastestPath.java
+++ src/myVelib/FastestPath.java
@@ -1,5 +1,6 @@
 package myVelib;
 
+import myVelib.Bicycle.BicycleType;
 import myVelib.Station.Station;
 
 public class FastestPath implements PathFinder {
@@ -9,9 +10,9 @@ public class FastestPath implements PathFinder {
 	
 
 	@Override
-	public void Path(GPS start, GPS end, MyVelib velibNW) {
+	public void Path(GPS start, GPS end, MyVelib velibNW, BicycleType type) {
 		
-		double d=0;
+		double t=0;
 		Station startS = null;
 		Station endS = null;
 		for (int i=0; i<velibNW.getDistanceMap().length ;i++) {
@@ -21,12 +22,15 @@ public class FastestPath implements PathFinder {
 				double di = Math.sqrt((start.getX()-velibNW.getStations().get(i).getPosition().getX())^2+(start.getY()-velibNW.getStations().get(i).getPosition().getY())^2);
 				double df = Math.sqrt((end.getX()-velibNW.getStations().get(j).getPosition().getX())^2+(end.getY()-velibNW.getStations().get(j).getPosition().getY())^2);
 				
-				if((d==0) || ((di+df+velibNW.getDistanceMap()[i][j] < d) && i!=j)) {
+				if (type.equals(BicycleType.ELECTRIC)){
 					
-					startS=velibNW.getStations().get(i);
-					endS=velibNW.getStations().get(j);
-					d = di+df+velibNW.getDistanceMap()[i][j];
+					if((t==0) || ((di+df)*9/10+velibNW.getDistanceMap()[i][j]*6/25 < t) && (i!=j) && (velibNW.getStations().get(j).freeParkingSpotsNb()!=0) && (velibNW.getStations().get(i).countBicycle(type)!=0)) {
 					
+						startS=velibNW.getStations().get(i);
+						endS=velibNW.getStations().get(j);
+						d = di+df+velibNW.getDistanceMap()[i][j];
+						
+					}
 				}
 			}
 		this.startStation=startS;
